e7c41a589111a083dc0f4c24bc5df07753db6a64
maledicente <luizpaulonievola@ymail.com>
2020-07-20 14:40:12 -0300

    Add Clion

---
+++ exercicios/cmake-build-debug/CMakeFiles/exercicios.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercicios.dir/main.c.o"
+  "exercicios"
+  "exercicios.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exercicios.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
